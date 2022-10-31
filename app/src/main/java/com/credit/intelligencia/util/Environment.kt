fa39c99efbf7cb64f0a573b90c736afefb387bf6
Stephen  Chacha <48447675+stevechacha@users.noreply.github.com>
2022-10-31 21:16:26 +0300

    Update Environment.kt

--- app/src/main/java/com/credit/intelligencia/util/Environment.kt
+++ app/src/main/java/com/credit/intelligencia/util/Environment.kt
@@ -1,5 +1,5 @@
 package com.credit.intelligencia.util
 
 sealed class Environment(val url: String) {
-    object Main : Environment("https://api-uat.equitygroupholdings.com/noncustomer/")
+    object Main : Environment("https://api.com/noncustomer/")
 }
